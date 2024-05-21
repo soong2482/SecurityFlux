430ecc16dbb9673431f3896d705ceb9c72bf1d9c
soong2482 <soong2482@naver.com>
2024-05-21 13:59:33 +0900

    Initial commit

---
+++ src/test/java/com/spring/SecurityFlux/SecurityFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SecurityFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
